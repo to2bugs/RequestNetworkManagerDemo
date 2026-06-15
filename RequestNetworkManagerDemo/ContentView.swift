b9986b8dd5eaa28d98e805f6f6a7f18f8647d21d
to2bugs <16759967+to2bugs@user.noreply.gitee.com>
2026-06-15 19:46:03 +0800

    Initial Commit

---
+++ RequestNetworkManagerDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RequestNetworkManagerDemo
+//
+//  Created by to2bage zero on 2026/6/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RequestNetworkManagerDemo/RequestNetworkManagerDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  RequestNetworkManagerDemoApp.swift
+//  RequestNetworkManagerDemo
+//
+//  Created by to2bage zero on 2026/6/15.
+//
+
+import SwiftUI
+
+@main
+struct RequestNetworkManagerDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
